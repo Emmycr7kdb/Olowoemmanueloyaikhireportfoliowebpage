f03ddefff2a56aa95ce52b238ac1fe0e58537312
Emmycr7kdb <bellarosefernadez@gmail.com>
2024-11-25 21:35:24 +0100

    Add files via upload

---
+++ javascript/meyawo.js
@@ -0,0 +1,12 @@
+
+const hack = window.prompt("Enter your name")
+if (hack != 0000)
+alertt = 'Enter your name'
+   window.alert ('Welcome to Olowo Emmanuel Web')
+
+   const mobileMenu = document.getElementById('nav-toggle');
+   const navLinks = document.getElementsByClassName('nav');
+
+   mobileMenu.addEventListener('click', () => {
+       navLinks.classList.toggle('active');
+   });
